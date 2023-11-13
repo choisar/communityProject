f7fc932c128bcda6c848dd8e886761af48c9372f
hsz0728 <150644140+hsz0728@users.noreply.github.com>
2023-11-13 14:23:57 +0900

    test1

---
+++ src/miniproject/test.java
@@ -0,0 +1,16 @@
+package miniproject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+}
